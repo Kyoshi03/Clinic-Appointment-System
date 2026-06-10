d5f301ff53263298454190257b5eb101b3f48f0e
Kyoshi03 <148612145+Kyoshi03@users.noreply.github.com>
2026-06-10 14:51:01 +0800

    Delete config/production.php

--- config/production.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'localhost',
-    'user' => 'u901773288_clinic1_user',
-    // Use the MySQL user password from Hostinger Databases > Management.
-    // This is not the Hostinger login, email, SMTP, or staff account password.
-    'pass' => 'H@nycheez0932',
-    'name' => 'u901773288_clinic1_db',
-];
